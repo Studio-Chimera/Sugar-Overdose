baaeb60cb55565c38a956984de9261f06404a3db
Lerenard <lerenard.charly@gmail.com>
2020-12-13 12:32:27 +0100

    feat: main menu with leave button, update resolution

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -42,7 +42,7 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(1920, 1080);
+static cocos2d::Size designResolutionSize = cocos2d::Size(1920, 1050);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
@@ -122,7 +122,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     // run
     director->runWithScene(scene);
-
+    
     return true;
 }
 
