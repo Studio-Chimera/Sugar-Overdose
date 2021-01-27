32398d40df29dc0c564b049c52f6423b78ef7d8f
FoxBandyKoot <32387837+FoxBandyKoot@users.noreply.github.com>
2021-01-27 15:55:19 +0100

    Update Level.cpp

--- Classes/utils/Level.cpp
+++ Classes/utils/Level.cpp
@@ -7,12 +7,11 @@ Scene* Level::scene()
     // 'scene' is an autorelease object
     Scene* scene = Scene::createWithPhysics();
     scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+    
     // 'layer' is an autorelease object
     Level* layer = Level::create();
     layer->createPhysicalWorld(scene->getPhysicsWorld());
-    // add layer as a child to scene
     scene->addChild(layer);
-    // return the scene
     return scene;
 }
 
@@ -34,9 +33,11 @@ void Level::setTiledMap(std::string map)
     // instanciate a new tiledMap with level.tmx as model
     _tileMap = new TMXTiledMap();
     _tileMap->initWithTMXFile(map);
+    
     // pickup tiles
     _background = _tileMap->layerNamed("background");
     _blocks = _tileMap->layerNamed("blocks");
+    
     // add the node to scene tree
     this->addChild(_tileMap);
 }
